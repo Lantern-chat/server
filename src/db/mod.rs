f8fc7d299e3a8c19bf4ab7f521b0a1b5bca9fdf0
novacrazy <novacrazy@gmail.com>
2020-12-17 11:01:49 -0600

    WIP query cache

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,12 +1,13 @@
-use tokio_postgres::Client;
-
+pub mod cache;
 pub mod conn;
 
 pub mod sf;
-
 pub use sf::Snowflake;
 
 pub mod schema {
+    use super::{cache::ClientExt, Snowflake};
+    use tokio_postgres::{Client, Error, Row};
+
     pub mod emote;
     pub mod invite;
     pub mod msg;
