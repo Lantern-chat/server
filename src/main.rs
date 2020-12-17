97cc1bd1d93e3eaa3bbfb21e1087dd07f7b3985a
novacrazy <novacrazy@gmail.com>
2020-12-16 23:12:21 -0600

    Set up shutdown signal for server and logging

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
+extern crate tracing as log;
+use tracing_subscriber::FmtSubscriber;
+
 pub mod db;
 pub mod routes;
 pub mod server;
 pub mod state;
 
-use slog::Drain;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+
+use futures::FutureExt;
 
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    //let root = slog::Logger::root(
-    //    Mutex::new(slog_json::Json::default(std::io::stderr())).map(slog::Fuse),
-    //    slog::o!("version" => env!("CARGO_PKG_VERSION")),
-    //);
+    // a builder for `FmtSubscriber`.
+    let subscriber = FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(log::Level::TRACE)
+        .finish(); // completes the builder.
 
-    server::start_server(Arc::new(state::ServerState {})).await;
+    log::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    let addr = ([127, 0, 0, 1], 3030);
+
+    let (snd, rcv) = tokio::sync::oneshot::channel();
+    let state = state::ServerState::new(snd);
+    let (addr, server) = warp::serve(routes::routes(state.clone()))
+        .bind_with_graceful_shutdown(addr, rcv.map(|_| { /* ignore errors */ }));
+
+    log::trace!("Setting up shutdown signal for Ctrl+C");
+    tokio::spawn(tokio::signal::ctrl_c().then(move |_| async move { state.shutdown().await }));
+
+    log::info!("Starting server...");
+    tokio::spawn(server).await;
 }
--- src/state.rs
+++ src/state.rs
@@ -1,7 +1,35 @@
 use std::sync::Arc;
 
-use tokio::sync::RwLock;
+use futures::FutureExt;
+use tokio::sync::{oneshot, Mutex, RwLock};
 
 use hashbrown::HashMap;
+use warp::hyper::Server;
 
-pub struct ServerState {}
+pub struct ServerState {
+    pub shutdown: Mutex<Option<oneshot::Sender<()>>>,
+}
+
+impl ServerState {
+    pub fn new(shutdown: oneshot::Sender<()>) -> Arc<Self> {
+        Arc::new(ServerState {
+            shutdown: Mutex::new(Some(shutdown)),
+        })
+    }
+
+    pub async fn shutdown(&self) {
+        match self.shutdown.lock().await.take() {
+            Some(shutdown) => {
+                log::debug!("Sending server shutdown signal");
+
+                if let Err(e) = shutdown.send(()) {
+                    tokio::spawn(
+                        tokio::time::sleep(std::time::Duration::from_secs(5))
+                            .map(|_| std::process::exit(1)),
+                    );
+                }
+            }
+            _ => {}
+        }
+    }
+}
