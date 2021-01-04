f2b8f174cae72a71d20b6f435313c402619e8cd7
novacrazy <novacrazy@gmail.com>
2021-01-04 11:03:37 -0600

    Put subscriptions with rooms migration

--- sql/migrations/3_create_room/down.sql
+++ sql/migrations/3_create_room/down.sql
@@ -1,3 +1,8 @@
+ALTER TABLE lantern.subscriptions DROP CONSTRAINT IF EXISTS user_fk CASCADE;
+ALTER TABLE lantern.subscriptions DROP CONSTRAINT IF EXISTS room_fk CASCADE;
+
+DROP TABLE IF EXISTS lantern.subscriptions;
+
 ALTER TABLE lantern.rooms DROP CONSTRAINT IF EXISTS party_fk CASCADE;
 
 DROP INDEX IF EXISTS lantern.room_name_idx CASCADE;
--- sql/migrations/3_create_room/up.sql
+++ sql/migrations/3_create_room/up.sql
@@ -16,3 +16,24 @@ CREATE INDEX CONCURRENTLY room_name_idx ON lantern.rooms USING hash (name);
 ALTER TABLE lantern.rooms ADD CONSTRAINT party_fk FOREIGN KEY (party_id)
     REFERENCES lantern.party (id) MATCH FULL
     ON DELETE CASCADE ON UPDATE CASCADE; -- Delete rooms if party is deleted
+
+
+CREATE TABLE lantern.subscriptions (
+    user_id         bigint NOT NULL,
+    room_id         bigint NOT NULL,
+    mentions        bool DEFAULT true,
+
+    -- If NULL, there is no mute
+    mute_expires    timestamp,
+
+    CONSTRAINT subscription_pk PRIMARY KEY (room_id, user_id)
+);
+ALTER TABLE lantern.subscriptions OWNER TO postgres;
+
+ALTER TABLE lantern.subscriptions ADD CONSTRAINT room_fk FOREIGN KEY (room_id)
+    REFERENCES lantern.rooms (id) MATCH FULL
+    ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE lantern.subscriptions ADD CONSTRAINT user_fk FOREIGN KEY (user_id)
+    REFERENCES lantern.users (id) MATCH FULL
+    ON DELETE CASCADE ON UPDATE CASCADE;
