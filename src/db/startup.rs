febc293e1c6a2e641f30357571b2b83cc4f4038f
novacrazy <novacrazy@gmail.com>
2021-01-23 13:01:34 -0600

    Fix SQL statement splitting

--- src/db/startup.rs
+++ src/db/startup.rs
@@ -107,7 +107,7 @@ pub async fn startup() -> anyhow::Result<Client> {
             let migration = load_migration(migration_path).await?;
 
             async fn run_batch(client: &Client, sql: &str) -> Result<(), anyhow::Error> {
-                for command in str::split(&sql, ";") {
+                for command in SqlIterator::new(&sql) {
                     client.execute(command, &[]).await?;
                 }
 
@@ -169,4 +169,134 @@ async fn load_migration(path: PathBuf) -> Result<Migration, anyhow::Error> {
         up: load_sql(up).await?,
         down: load_sql(down).await?,
     })
+}
+
+struct SqlIterator<'a> {
+    sql: &'a str,
+}
+
+impl<'a> SqlIterator<'a> {
+    pub fn new(sql: &'a str) -> Self {
+        SqlIterator { sql }
+    }
+}
+
+impl<'a> Iterator for SqlIterator<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<&'a str> {
+        let mut in_dollar = false;
+        let mut ic = self.sql.char_indices().peekable();
+
+        loop {
+            if let Some((idx, c)) = ic.next() {
+                if c == '$' && ic.peek().map(|(_, c)| *c == '$') == Some(true) {
+                    in_dollar = !in_dollar;
+                }
+
+                if c == ';' && !in_dollar {
+                    let res = Some(&self.sql[..idx]);
+                    self.sql = &self.sql[idx + 1..];
+                    return res;
+                }
+            } else {
+                return None;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static QUERY_A: &str = r#"
+
+CREATE TABLE lantern.users (
+    --- Snowflake id
+    id              bigint              NOT NULL,
+    deleted_at      timestamp,
+    username        varchar(64)         NOT NULL,
+    -- 2-byte integer that can be displayed as 4 hex digits
+    discriminator   smallint            NOT NULL,
+    email           text                NOT NULL,
+    dob             date                NOT NULL,
+    is_verified     bool                NOT NULL    DEFAULT false,
+    passhash        text                NOT NULL,
+    nickname        varchar(256),
+    -- custom_status tracks the little blurb that appears on users
+    custom_status   varchar(128),
+    -- biography is an extended user description on their profile
+    biography       varchar(1024),
+    -- this is for client-side user preferences, which can be stored as JSON easily enough
+    preferences     jsonb,
+
+    -- 0/NULL for online, 1 for away, 2 for busy, 3 for invisible
+    away            smallint,
+
+    CONSTRAINT users_pk PRIMARY KEY (id)
+);
+ALTER TABLE lantern.users OWNER TO postgres;
+
+-- Fast lookup of users with identical usernames
+CREATE INDEX CONCURRENTLY user_username_idx ON lantern.users
+    USING hash (username);
+
+-- Fast lookup of users via `username#0000`
+CREATE INDEX CONCURRENTLY user_username_discriminator_idx ON lantern.users
+    USING btree (username, discriminator);
+
+CREATE TABLE lantern.users_freelist (
+    username        varchar(64) NOT NULL,
+    descriminator   smallint    NOT NULL
+);
+ALTER TABLE lantern.users_freelist OWNER TO postgres;
+
+CREATE INDEX CONCURRENTLY user_freelist_username_idx ON lantern.users_freelist
+    USING hash (username);
+
+    "#;
+
+    static QUERY_B: &str = r#"
+    CREATE OR REPLACE PROCEDURE lantern.register_user(
+        _id bigint,
+        _username varchar(64),
+        _email text,
+        _passhash text,
+        _dob date
+     )
+     LANGUAGE plpgsql AS
+     $$
+     DECLARE
+        _discriminator smallint;
+     BEGIN
+         SELECT descriminator INTO _discriminator FROM lantern.users_freelist WHERE username = _username;
+
+         IF FOUND THEN
+             DELETE FROM lantern.users_freelist WHERE username = _username AND discriminator = _discriminator;
+         ELSE
+             SELECT discriminator INTO _discriminator FROM lantern.users WHERE username = _username ORDER BY id DESC;
+
+             IF NOT FOUND THEN
+                 _discriminator := 0;
+             ELSIF _discriminator = -1 THEN
+                 RAISE EXCEPTION 'Username % exhausted', _username;
+             ELSE
+                 _discriminator := _discriminator + 1;
+             END IF;
+         END IF;
+
+         INSERT INTO lantern.users (id, username, discriminator, email, passhash, dob) VALUES (_id, _username, _discriminator, _email, _passhash, _dob);
+     END
+     $$;
+
+    "#;
+
+    #[test]
+    fn test_split_a() {
+        for part in SqlIterator::new(QUERY_A).take(20) {
+            println!("{};", part);
+            println!("------------------------------");
+        }
+    }
 }
