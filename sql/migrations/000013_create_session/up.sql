f599d2fa028b7f974e4fd05dc6441d6bd3bca09b
novacrazy <novacrazy@gmail.com>
2021-01-26 02:54:24 -0600

    Tweak session 2

--- sql/migrations/000013_create_session/up.sql
+++ sql/migrations/000013_create_session/up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE lantern.sessions (
-    id      bytea       NOT NULL,
+    token   bytea       NOT NULL,
     user_id bigint      NOT NULL,
     expires timestamp   NOT NULL
 );
 ALTER TABLE lantern.sessions OWNER TO postgres;
 
-CREATE INDEX CONCURRENTLY session_id_idx ON lantern.sessions
-    USING hash (id);
+CREATE INDEX CONCURRENTLY session_token_idx ON lantern.sessions
+    USING hash (token);
 
 CREATE INDEX CONCURRENTLY session_expires_idx ON lantern.sessions
     USING btree (expires);
