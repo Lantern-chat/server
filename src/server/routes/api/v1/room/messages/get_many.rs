ad7710fddb0d9f2cd77f139555584e099c049963
novacrazy <novacrazy@gmail.com>
2021-03-01 05:38:41 -0600

    Implement get_many messages

--- src/server/routes/api/v1/room/messages/get_many.rs
+++ src/server/routes/api/v1/room/messages/get_many.rs
@@ -16,7 +16,24 @@ pub struct GetManyMessagesForm {
     #[serde(flatten)]
     query: MessageSearch,
 
+    #[serde(default = "default_limit")]
     limit: u8,
 }
 
-pub async fn get_many(mut route: Route, auth: Authorization, room_id: Snowflake) -> impl Reply {}
+#[rustfmt::skip]
+const fn default_limit() -> u8 { 50 }
+
+pub async fn get_many(mut route: Route, auth: Authorization, room_id: Snowflake) -> impl Reply {
+    let form = match body::any::<GetManyMessagesForm>(&mut route).await {
+        Ok(form) => form,
+        Err(e) => return e.into_response(),
+    };
+
+    match Message::search(&route.state.db, room_id, form.limit, form.query).await {
+        Ok(msg) => reply::json_stream(msg).into_response(),
+        Err(e) => {
+            log::error!("Error getting many messsages: {}", e);
+            StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        }
+    }
+}
