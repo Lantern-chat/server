fc8776f643195a4afecb6ed415bc68e11001e0a6
novacrazy <novacrazy@gmail.com>
2021-03-01 22:09:16 -0600

    Add party_member stuff

--- src/db/schema/party.rs
+++ src/db/schema/party.rs
@@ -1,3 +1,5 @@
+use futures::{Stream, StreamExt};
+
 use super::*;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -5,6 +7,26 @@ pub struct Party {
     pub id: Snowflake,
     pub owner_id: Snowflake,
     pub name: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PartyMember {
+    pub id: Snowflake,
+    pub username: String,
+    pub discriminator: i16,
+    pub is_verified: bool,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub nickname: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub status: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub bio: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub avatar_id: Option<Snowflake>,
 }
 
 impl Party {
@@ -35,6 +57,45 @@ impl Party {
                 name: row.try_get(1)?,
             })),
         }
+    }
+
+    pub async fn get_members(
+        &self,
+        client: &Client,
+    ) -> Result<impl Stream<Item = Result<PartyMember, ClientError>>, ClientError> {
+        let stream = client.query_stream_cached(
+            || "SELECT id, username, discriminator, is_verified, COALESCE(party_member.nickname, users.nickname), custom_status, biography, COALESCE(party_member.avatar_id, users.avatar_id) FROM users LEFT JOIN party_member ON id = user_id WHERE party_id = $1",
+            &[&self.id],
+        ).await?;
+
+        Ok(stream.map(|res| match res {
+            Err(e) => Err(e),
+            Ok(row) => Ok(PartyMember {
+                id: row.try_get(0)?,
+                username: row.try_get(1)?,
+                discriminator: row.try_get(2)?,
+                is_verified: row.try_get(3)?,
+                nickname: row.try_get(4)?,
+                status: row.try_get(5)?,
+                bio: row.try_get(6)?,
+                avatar_id: row.try_get(7)?,
+            }),
+        }))
+    }
+
+    pub async fn has_member(
+        &self,
+        user_id: Snowflake,
+        client: &Client,
+    ) -> Result<bool, ClientError> {
+        let count = client
+            .execute_cached(
+                || "SELECT FROM lantern.party_member WHERE party_id = $1 AND user_id = $2",
+                &[&self.id, &user_id],
+            )
+            .await?;
+
+        Ok(count > 0)
     }
 
     /*
--- src/db/schema/user.rs
+++ src/db/schema/user.rs
@@ -1,13 +1,19 @@
 use super::*;
 
+use time::PrimitiveDateTime;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct User {
     pub id: Snowflake,
     pub username: String,
+    pub discriminator: i16,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub email: Option<String>,
     pub nickname: Option<String>,
     pub blurb: Option<String>,
     pub avatar_id: Option<Snowflake>,
-    pub preferences: String, // JSON
+    pub preferences: Option<String>, // JSON
 }
 
 impl User {}
