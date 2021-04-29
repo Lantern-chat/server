ae40fd155a30139568a0ee52755a84a3cd2d8a8e
novacrazy <novacrazy@gmail.com>
2021-04-29 08:03:17 -0500

    Add overrides migration

---
+++ sql/migrations/000017_create_overrides/up.sql
@@ -0,0 +1,25 @@
+CREATE TABLE lantern.overrides (
+    room_id         bigint      NOT NULL,
+
+    role_id         bigint,
+    user_id         bigint,
+
+    allow           bigint      NOT NULL    DEFAULT 0,
+    deny            bigint      NOT NULL    DEFAULT 0,
+);
+ALTER TABLE lantern.overrides OWNER TO postgres;
+
+CREATE INDEX CONCURRENTLY overrides_room_role_idx ON lantern.overrides USING hash(room_id, role_id);
+CREATE INDEX CONCURRENTLY overrides_room_user_idx ON lantern.overrides USING hash(room_id, user_id);
+
+ALTER TABLE lantern.overrides ADD CONSTRAINT room_id_fk FOREIGN KEY (room_id)
+    REFERENCES lantern.rooms (id) MATCH FULL
+    ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE lantern.overrides ADD CONSTRAINT role_id_fk FOREIGN KEY (role_id)
+    REFERENCES lantern.roles (id) MATCH FULL
+    ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE lantern.overrides ADD CONSTRAINT user_id_fk FOREIGN KEY (user_id)
+    REFERENCES lantern.users (id) MATCH FULL
+    ON DELETE CASCADE ON UPDATE CASCADE;
