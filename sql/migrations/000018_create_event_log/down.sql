3eb2eeb0c6fcabd48a6e8aa630e5319faeeccdd7
novacrazy <novacrazy@gmail.com>
2021-05-25 04:42:32 -0500

    Implement ev_notify rate-limiting

--- sql/migrations/000018_create_event_log/down.sql
+++ sql/migrations/000018_create_event_log/down.sql
@@ -1,3 +1,8 @@
-DROP INDEX IF EXISTS lantern.event_log_idx CASCADE;
+DROP TRIGGER event_log_notify ON lantern.event_log_last_notification CASCADE;
+DROP FUNCTION IF EXISTS lantern.ev_notify() CASCADE;
 
+DROP TABLE IF EXISTS lantern.event_log_last_notification CASCADE;
+
+DROP INDEX IF EXISTS lantern.event_log_idx CASCADE;
+ALTER TABLE IF EXISTS lantern.roles DROP CONSTRAINT IF EXISTS party_fk CASCADE;
 DROP TABLE IF EXISTS lantern.event_log CASCADE;
--- sql/migrations/000018_create_event_log/up.sql
+++ sql/migrations/000018_create_event_log/up.sql
@@ -1,7 +1,48 @@
 CREATE TABLE lantern.event_log (
-    code    smallint    NOT NULL,
-    id      bigint      NOT NULL
+    code        smallint    NOT NULL,
+    party_id    bigint,
+    id          bigint      NOT NULL
 );
 ALTER TABLE lantern.event_log OWNER TO postgres;
 
+ALTER TABLE lantern.event_log ADD CONSTRAINT party_fk FOREIGN KEY (party_id)
+    REFERENCES lantern.party (id) MATCH FULL
+    ON DELETE CASCADE ON UPDATE CASCADE;
+
 CREATE INDEX event_log_idx ON lantern.event_log USING btree(id);
+
+
+
+
+CREATE TABLE lantern.event_log_last_notification (
+    last_notif timestamp NOT NULL DEFAULT now(),
+    max_interval interval NOT NULL DEFAULT INTERVAL '100 milliseconds'
+);
+
+CREATE OR REPLACE FUNCTION ev_notify()
+RETURNS trigger
+LANGUAGE plpgsql AS
+$$
+DECLARE
+    _last_notif timestamp;
+    _max_interval interval;
+    _now timestamp := now();
+BEGIN
+    SELECT
+        last_notif, max_interval
+    INTO
+        _last_notif, _max_interval
+    FROM lantern.event_log_last_notification;
+
+    IF age(_now, _last_notif) < _max_interval THEN
+        CALL pg_notify('event_log', (NEW.id)::text);
+        UPDATE lantern.event_log_last_notification SET
+            last_notif = _now;
+    END IF;
+    RETURN NEW;
+END
+$$;
+
+CREATE TRIGGER event_log_notify AFTER INSERT ON lantern.event_log
+FOR EACH ROW
+EXECUTE FUNCTION lantern.ev_notify();
