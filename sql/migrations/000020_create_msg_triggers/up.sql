a59698a79b69d5690e32e632d5eb4a3d523c6031
novacrazy <novacrazy@gmail.com>
2021-06-06 09:55:07 -0500

    Simplify msg_trigger

--- sql/migrations/000020_create_msg_triggers/up.sql
+++ sql/migrations/000020_create_msg_triggers/up.sql
@@ -2,26 +2,15 @@ CREATE OR REPLACE FUNCTION lantern.msg_trigger()
 RETURNS trigger
 LANGUAGE plpgsql AS
 $$
-DECLARE
-    _code smallint;
-    _party_id bigint;
-    _id bigint;
 BEGIN
-    IF OLD IS NOT NULL AND
-       OLD.flags != NEW.flags AND
-       (NEW.flags & (1 << 5) != 0)
-    THEN
-        _code := 3;
-    ELSE
-        _code := CASE WHEN TG_OP = 'INSERT' THEN 1
-                      WHEN TG_OP = 'UPDATE' THEN 2
-                 END;
-    END IF;
-
-    _party_id := (SELECT party_id FROM lantern.rooms WHERE rooms.id = NEW.room_id);
-    _id := NEW.id;
-
-    INSERT INTO lantern.event_log (code, id, party_id) VALUES(_code, _id, _party_id);
+    INSERT INTO lantern.event_log (code, id, party_id)
+    SELECT
+        CASE WHEN OLD IS NOT NULL AND OLD.flags != NEW.flags AND (NEW.flags & (1 << 5) != 0) THEN 3
+             WHEN TG_OP = 'INSERT' THEN 1
+             WHEN TG_OP = 'UPDATE' THEN 2
+        END,
+        NEW.id,
+        (SELECT party_id FROM lantern.rooms WHERE rooms.id = NEW.room_id);
 
     RETURN NEW;
 END
