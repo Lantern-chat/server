c50a071c68d2ee030479d2016d64b51e0cec2a01
novacrazy <novacrazy@gmail.com>
2021-07-05 11:46:02 -0500

    Cleanup

--- crates/db/src/lib.rs
+++ crates/db/src/lib.rs
@@ -9,6 +9,5 @@ extern crate tracing as log;
 
 pub mod migrate;
 pub mod pool;
-pub mod util;
 
 pub use pg::Row;
--- crates/db/src/util.rs
+++
@@ -1,4 +0,0 @@
-#[inline]
-pub const fn is_false(value: &bool) -> bool {
-    !*value
-}
