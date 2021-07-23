3076519f851570d4d4cdd1f29ad07a97bed1830e
novacrazy <novacrazy@gmail.com>
2021-07-22 20:36:31 -0500

    WIP

--- crates/server/src/filesystem/mod.rs
+++ crates/server/src/filesystem/mod.rs
@@ -1,3 +1,3 @@
 pub mod crypt;
 pub mod path;
-pub mod disk;
+pub mod store;
---
+++ crates/server/src/filesystem/store.rs
@@ -0,0 +1,82 @@
+use std::io::{self, SeekFrom};
+use std::path::{Path, PathBuf};
+use std::pin::Pin;
+
+use aes::cipher::{BlockCipherKey, NewCipher, Nonce};
+use aes::{Aes256, Aes256Ctr, BlockCipher, NewBlockCipher};
+
+use tokio::fs::{self, File as TkFile, OpenOptions};
+use tokio::io::{AsyncRead, AsyncSeek, AsyncWrite};
+
+use schema::Snowflake;
+
+use crate::filesystem::path::{id_to_name, id_to_path};
+
+pub struct FileStore {
+    pub root: PathBuf,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum OpenMode {
+    Read,
+    Write,
+}
+
+pub struct CipherOptions {
+    pub key: BlockCipherKey<Aes256>,
+    pub nonce: Nonce<Aes256Ctr>,
+}
+
+impl CipherOptions {
+    pub fn create(&self) -> Aes256Ctr {
+        Aes256Ctr::new(&self.key, &self.nonce)
+    }
+}
+
+use super::crypt::EncryptedFile;
+
+pub trait RWSeekStream: AsyncWrite + AsyncRead + AsyncSeek + Send {}
+impl<T> RWSeekStream for T where T: AsyncWrite + AsyncRead + AsyncSeek + Send {}
+
+impl FileStore {
+    pub fn new<P: AsRef<Path>>(root: P) -> FileStore {
+        FileStore {
+            root: root.as_ref().to_owned(),
+        }
+    }
+
+    pub async fn open_crypt(
+        &self,
+        id: Snowflake,
+        mode: OpenMode,
+        options: &CipherOptions,
+    ) -> io::Result<Pin<Box<dyn RWSeekStream>>> {
+        let cipher = options.create();
+        let file = self.open(id, mode).await?;
+
+        Ok(match mode {
+            OpenMode::Read => Box::pin(EncryptedFile::new(file, cipher)),
+            // write-mode has some extra optimizations for buffering encrypted writes
+            OpenMode::Write => Box::pin(EncryptedFile::new_write(file, cipher)),
+        })
+    }
+
+    pub async fn open(&self, id: Snowflake, mode: OpenMode) -> io::Result<TkFile> {
+        let mut path = self.root.clone();
+        id_to_path(id, &mut path);
+
+        if mode == OpenMode::Write {
+            fs::create_dir_all(&path).await?;
+        }
+
+        id_to_name(id, &mut path);
+
+        let mut options = OpenOptions::new();
+        let _ = match mode {
+            OpenMode::Read => options.read(true),
+            OpenMode::Write => options.write(true).create(true),
+        };
+
+        options.open(path).await
+    }
+}
