8d336b96ebffdc9acc674d5a9ab32faf53987e66
novacrazy <novacrazy@gmail.com>
2021-09-22 21:35:21 -0500

    Add lantern.update_user

--- sql/migrations/000012_register_user/up.sql
+++ sql/migrations/000012_register_user/up.sql
@@ -8,18 +8,18 @@ CREATE OR REPLACE PROCEDURE lantern.register_user(
 LANGUAGE plpgsql AS
 $$
 DECLARE
-   _discriminator smallint;
+   _discriminator uint2;
 BEGIN
-    SELECT descriminator INTO _discriminator FROM lantern.users_freelist WHERE username = _username;
+    SELECT discriminator INTO _discriminator FROM lantern.user_freelist WHERE username = _username;
 
     IF FOUND THEN
-        DELETE FROM lantern.users_freelist WHERE username = _username AND discriminator = _discriminator;
+        DELETE FROM lantern.user_freelist WHERE username = _username AND discriminator = _discriminator;
     ELSE
-        SELECT discriminator INTO _discriminator FROM lantern.users WHERE username = _username ORDER BY id DESC;
+        SELECT discriminator INTO _discriminator FROM lantern.users WHERE username = _username ORDER BY discriminator DESC LIMIT 1;
 
         IF NOT FOUND THEN
             _discriminator := 0;
-        ELSIF _discriminator = -1 THEN
+        ELSIF _discriminator = 65535 THEN
             RAISE EXCEPTION 'Username % exhausted', _username;
         ELSE
             _discriminator := _discriminator + 1;
@@ -27,5 +27,47 @@ BEGIN
     END IF;
 
     INSERT INTO lantern.users (id, username, discriminator, email, passhash, dob) VALUES (_id, _username, _discriminator, _email, _passhash, _dob);
+END
+$$;
+
+CREATE OR REPLACE PROCEDURE lantern.update_user(
+    _id bigint,
+    _username varchar(64),
+    _email text,
+    _passhash text
+)
+LANGUAGE plpgsql AS
+$$
+DECLARE
+    _discriminator uint2;
+BEGIN
+    IF _username IS NOT NULL THEN
+        SELECT discriminator INTO _discriminator FROM lantern.user_freelist WHERE username = _username;
+
+        IF FOUND THEN
+            DELETE FROM lantern.user_freelist WHERE username = _username AND discriminator = _discriminator;
+        ELSE
+            SELECT discriminator INTO _discriminator FROM lantern.users WHERE username = _username ORDER BY discriminator DESC LIMIT 1;
+
+            IF NOT FOUND THEN
+                _discriminator := 0;
+            ELSIF _discriminator = 65535 THEN
+                RAISE EXCEPTION 'Username % exhausted', _username;
+            ELSE
+                _discriminator := _discriminator + 1;
+            END IF;
+        END IF;
+
+        -- Add current user's username to the freelist once found
+        INSERT INTO lantern.user_freelist (SELECT username, discriminator FROM lantern.users WHERE users.id = _id);
+    END IF;
+
+    UPDATE lantern.users SET
+        username        = COALESCE(_username,       username),
+        discriminator   = COALESCE(_discriminator,  discriminator),
+        email           = COALESCE(_email,          email),
+        passhash        = COALESCE(_passhash,       passhash)
+    WHERE
+        users.id = _id;
 END
 $$;
