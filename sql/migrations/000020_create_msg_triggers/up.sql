2702cff33d18eb21eab4c9e552103083ceb5559f
novacrazy <novacrazy@gmail.com>
2021-10-08 15:58:37 -0500

    Fix message delete trigger

--- sql/migrations/000020_create_msg_triggers/up.sql
+++ sql/migrations/000020_create_msg_triggers/up.sql
@@ -5,10 +5,15 @@ $$
 BEGIN
     INSERT INTO lantern.event_log (code, id, party_id)
     SELECT
-        CASE WHEN OLD IS NOT NULL AND OLD.flags != NEW.flags AND (NEW.flags & (1 << 5) != 0)
-                                   THEN 'message_delete'::lantern.event_code
-             WHEN TG_OP = 'INSERT' THEN 'message_create'::lantern.event_code
-             WHEN TG_OP = 'UPDATE' THEN 'message_update'::lantern.event_code
+        -- when old was not deleted, and new is deleted
+        CASE WHEN ((OLD.flags & (1 << 5)) = 0) AND ((NEW.flags & (1 << 5)) != 0)
+                THEN 'message_delete'::lantern.event_code
+
+             WHEN TG_OP = 'INSERT'
+                THEN 'message_create'::lantern.event_code
+
+             WHEN TG_OP = 'UPDATE' AND ((NEW.flags & (1 << 5)) = 0)
+                THEN 'message_update'::lantern.event_code
         END,
         NEW.id,
         (SELECT party_id FROM lantern.rooms WHERE rooms.id = NEW.room_id);
@@ -17,5 +22,7 @@ BEGIN
 END
 $$;
 
+
+DROP TRIGGER IF EXISTS message_event on lantern.messages CASCADE;
 CREATE TRIGGER message_event AFTER UPDATE OR INSERT ON lantern.messages
 FOR EACH ROW EXECUTE FUNCTION lantern.msg_trigger();
