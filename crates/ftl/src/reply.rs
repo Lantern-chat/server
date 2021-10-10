7e1cace2598a352c94f4d63106b42e7656c81f29
novacrazy <novacrazy@gmail.com>
2021-10-10 16:29:08 -0500

    Streaming JSON Map reply

--- crates/ftl/src/reply.rs
+++ crates/ftl/src/reply.rs
@@ -161,7 +161,80 @@ pub struct JsonStream {
     body: Body,
 }
 
-pub fn json_stream<T, E>(stream: impl Stream<Item = Result<T, E>> + Send + 'static) -> impl Reply
+pub fn json_map_stream<K, T, E>(stream: impl Stream<Item = Result<(K, T), E>> + Send + 'static) -> impl Reply
+where
+    K: AsRef<str>,
+    T: serde::Serialize + Send + Sync + 'static,
+    E: Into<Box<dyn std::error::Error + Send + Sync>> + Send + Sync + 'static,
+{
+    let (mut sender, body) = Body::channel();
+
+    tokio::spawn(async move {
+        futures::pin_mut!(stream);
+
+        let mut first = true;
+        let mut buffer = String::with_capacity(128);
+        buffer.push('{');
+
+        let error: Result<(), Box<dyn std::error::Error + Send + Sync>> = loop {
+            match stream.next().await {
+                Some(Ok((key, ref value))) => {
+                    let pos = buffer.len();
+
+                    // most keys will be well-behaved and not need escaping, so `,"key":`
+                    // extra byte won't hurt anything when the value is serialized
+                    buffer.reserve(key.as_ref().len() + 4);
+
+                    if !first {
+                        buffer.push_str(",\"");
+                    } else {
+                        buffer.push('"');
+                    }
+
+                    use std::fmt::Write;
+                    if let Err(e) = write!(buffer, "{}", v_jsonescape::escape(key.as_ref())) {
+                        buffer.truncate(pos); // revert back to previous element
+                        break Err(e.into());
+                    }
+
+                    buffer.push_str("\":");
+
+                    if let Err(e) = serde_json::to_writer(unsafe { buffer.as_mut_vec() }, value) {
+                        buffer.truncate(pos); // revert back to previous element
+                        break Err(e.into());
+                    }
+
+                    first = false;
+                }
+                Some(Err(e)) => break Err(e.into()),
+                None => break Ok(()),
+            }
+
+            // Flush buffer at 4KB
+            if buffer.len() >= 4096 {
+                let chunk = Bytes::from(std::mem::replace(&mut buffer, String::new()));
+                if let Err(e) = sender.send_data(chunk).await {
+                    log::error!("Error sending JSON map chunk: {}", e);
+                    return sender.abort();
+                }
+            }
+        };
+
+        buffer.push('}');
+        if let Err(e) = sender.send_data(buffer.into()).await {
+            log::error!("Error sending JSON map chunk: {}", e);
+            return sender.abort();
+        }
+
+        if let Err(e) = error {
+            log::error!("Error serializing json map: {}", e);
+        }
+    });
+
+    JsonStream { body }
+}
+
+pub fn json_array_stream<T, E>(stream: impl Stream<Item = Result<T, E>> + Send + 'static) -> impl Reply
 where
     T: serde::Serialize + Send + Sync + 'static,
     E: Into<Box<dyn std::error::Error + Send + Sync>> + Send + Sync + 'static,
