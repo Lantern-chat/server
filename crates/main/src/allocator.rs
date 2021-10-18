6e968e180b19d4c7a130b144f100f2c517c48b9d
novacrazy <novacrazy@gmail.com>
2021-10-18 02:17:56 -0500

    Use jemalloc on *nix

--- crates/main/src/allocator.rs
+++ crates/main/src/allocator.rs
@@ -1,14 +1,18 @@
-use std::alloc::{GlobalAlloc, Layout, System};
-
-struct MyAllocator;
+use std::alloc::{GlobalAlloc, Layout};
 
 use server::metric::MEMORY_METRICS;
 
 use util::likely::likely;
 
+#[cfg(not(unix))]
+type Global = std::alloc::System;
+#[cfg(unix)]
+type Global = tikv_jemallocator::Jemalloc;
+
+struct MyAllocator;
 unsafe impl GlobalAlloc for MyAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let ret = System.alloc(layout);
+        let ret = Global {}.alloc(layout);
 
         if likely(!ret.is_null()) {
             // MEMORY_METRICS.allocs.add(1);
@@ -22,11 +26,11 @@ unsafe impl GlobalAlloc for MyAllocator {
         // MEMORY_METRICS.deallocs.add(1);
         MEMORY_METRICS.allocated.sub(layout.size() as u64);
 
-        System.dealloc(ptr, layout)
+        Global {}.dealloc(ptr, layout)
     }
 
     unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
-        let ret = System.alloc_zeroed(layout);
+        let ret = Global {}.alloc_zeroed(layout);
 
         if likely(!ret.is_null()) {
             // MEMORY_METRICS.allocs.add(1);
@@ -39,7 +43,7 @@ unsafe impl GlobalAlloc for MyAllocator {
     unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
         MEMORY_METRICS.allocated.sub(layout.size() as u64);
 
-        let ret = System.realloc(ptr, layout, new_size);
+        let ret = Global {}.realloc(ptr, layout, new_size);
 
         if likely(!ret.is_null()) {
             MEMORY_METRICS.allocated.add(new_size as u64);
