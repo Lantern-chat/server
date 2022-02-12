ed68eb7ab8d713ff7662fbd3a263daca0eb4fa2a
novacrazy <novacrazy@gmail.com>
2022-02-12 09:31:39 -0600

    Return simple Snowflake from POST file request

--- crates/server/src/web/routes/api/v1/file/post.rs
+++ crates/server/src/web/routes/api/v1/file/post.rs
@@ -9,7 +9,7 @@ use crate::{
 };
 
 pub async fn post(mut route: Route<ServerState>, auth: Authorization) -> Response {
-    let body = match body::any::<FilePostBody, _>(&mut route).await {
+    let body = match body::any(&mut route).await {
         Ok(body) => body,
         Err(e) => return ApiError::err(e.into()).into_response(),
     };
@@ -17,8 +17,9 @@ pub async fn post(mut route: Route<ServerState>, auth: Authorization) -> Respons
     match crate::ctrl::file::post::post_file(route.state.clone(), auth.user_id, body).await {
         Err(e) => ApiError::err(e).into_response(),
         Ok(file_id) => {
-            let mut res = Response::default();
-            *res.status_mut() = StatusCode::CREATED;
+            let mut res = reply::json(&file_id)
+                .with_status(StatusCode::CREATED)
+                .into_response();
 
             res.headers_mut()
                 .extend(super::TUS_HEADERS.iter().map(|(k, v)| (k.clone(), v.clone())));
