b4639b0e6d980420fe8e44401abf51c24239be23
novacrazy <novacrazy@gmail.com>
2022-02-26 20:06:51 -0600

    Improve bot token generation

--- crates/schema/src/auth/bot.rs
+++ crates/schema/src/auth/bot.rs
@@ -1,37 +1,45 @@
-use hmac::{digest::Key, Mac, SimpleHmac};
+use std::time::SystemTime;
+
+use hmac::{
+    digest::{FixedOutput, Key},
+    Mac, SimpleHmac,
+};
 use sha1::Sha1;
-
-type Sha1Hmac = SimpleHmac<Sha1>;
-
-pub type BotTokenKey = Key<Sha1Hmac>;
 
 use sdk::models::{Snowflake, SplitBotToken};
 
+type Sha1Hmac = SimpleHmac<Sha1>;
+pub type BotTokenKey = Key<Sha1Hmac>;
+
 pub trait SplitBotTokenExt {
-    fn new(key: &BotTokenKey, id: Snowflake, ts: u64) -> Self;
+    /// Generates a new bot token at this time
+    fn new(key: &BotTokenKey, id: Snowflake) -> Self;
     fn verify(&self, key: &BotTokenKey) -> bool;
 }
 
+fn token_mac(token: &SplitBotToken, key: &BotTokenKey) -> Sha1Hmac {
+    let mut mac = Sha1Hmac::new(key);
+    mac.update(&token.to_bytes()[0..16]);
+    mac
+}
+
 impl SplitBotTokenExt for SplitBotToken {
-    fn new(key: &BotTokenKey, id: Snowflake, ts: u64) -> Self {
+    fn new(key: &BotTokenKey, id: Snowflake) -> Self {
         let mut t = SplitBotToken {
             id,
-            ts,
             hmac: [0; 20],
+            ts: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs(),
         };
 
-        t.hmac = {
-            let mut mac = Sha1Hmac::new(key);
-            mac.update(&t.to_bytes()[0..16]);
-            mac.finalize().into_bytes().into()
-        };
+        t.hmac = token_mac(&t, key).finalize_fixed().into();
 
         t
     }
 
     fn verify(&self, key: &BotTokenKey) -> bool {
-        let mut mac = Sha1Hmac::new(key);
-        mac.update(&self.to_bytes()[0..16]);
-        mac.verify_slice(&self.hmac).is_ok()
+        token_mac(self, key).verify_slice(&self.hmac).is_ok()
     }
 }
