72a997e301c84f427ac0e68673c36e1b0730ba43
novacrazy <novacrazy@gmail.com>
2022-07-02 14:54:12 -0500

    Make compute_edge_info work with RGBA as well

--- crates/processing/src/heuristic.rs
+++ crates/processing/src/heuristic.rs
@@ -1,4 +1,4 @@
-use image::{Rgb, RgbImage};
+use image::{GenericImageView, Pixel};
 
 #[derive(Debug, Clone, Copy)]
 pub struct EdgeInfo {
@@ -7,18 +7,23 @@ pub struct EdgeInfo {
 }
 
 impl EdgeInfo {
+    // NOTE: This is a guestimate
     pub fn use_lossy(&self) -> bool {
-        // NOTE: This is a guestimate
-        self.average < 0.03 && self.variance < 0.008
+        // try to avoid images that have many lines
+        self.average < 0.035 && self.variance < 0.013
+        // but if it has very little high-freq details, use PNG
+        && (self.average > 0.005 || self.variance < 0.001)
     }
 }
 
 /// Fast 3x3 image convolution, same as image's filter3x3 but for RGB_8 only
 /// and without any intermediate buffers, outputting to a callback function instead
 #[inline]
-fn apply_kernel<F>(image: &RgbImage, mut kernel: [f32; 9], mut cb: F)
+fn apply_kernel<I, P, F>(image: &I, mut kernel: [f32; 9], mut cb: F)
 where
-    F: FnMut(u32, u32, Rgb<f32>),
+    I: GenericImageView<Pixel = P>,
+    P: Pixel<Subpixel = u8>,
+    F: FnMut(u32, u32, [f32; 3]),
 {
     const TAPS: &[(isize, isize)] = &[
         (-1, -1),
@@ -47,9 +52,9 @@ where
                 let x0 = x as isize + a;
                 let y0 = y as isize + b;
 
-                let rgb = image.get_pixel(x0 as u32, y0 as u32).0;
+                let p = image.get_pixel(x0 as u32, y0 as u32);
 
-                for (&c, f) in rgb.iter().zip(&mut t) {
+                for (&c, f) in p.channels().iter().zip(&mut t) {
                     *f += k * c as f32;
                 }
             }
@@ -58,38 +63,45 @@ where
                 *tc = tc.clamp(0.0, 1.0);
             }
 
-            cb(x, y, Rgb(t));
+            cb(x, y, t);
         }
     }
 }
 
-pub fn compute_edge_info(image: &RgbImage) -> EdgeInfo {
-    // RGB, so 3 channels.
-    let num_pixels = (image.as_raw().len() / 3) as f64;
-    let weight = 1.0 / num_pixels;
+/// Laplacian operator with diagonals
+#[rustfmt::skip]
+const LAPLACIAN: [f32; 9] = [
+    -1.0, -1.0, -1.0,
+    -1.0,  8.0, -1.0,
+    -1.0, -1.0, -1.0,
+];
+
+pub fn compute_edge_info<I, P>(image: &I) -> EdgeInfo
+where
+    I: GenericImageView<Pixel = P>,
+    P: Pixel<Subpixel = u8>,
+{
+    let (width, height) = image.dimensions();
+    let n = (width as u64 * height as u64) as f64;
+    let weight = 1.0 / n;
 
     let mut average = 0.0;
     let mut sumsq = 0.0;
 
-    apply_kernel(
-        image,
-        // Laplacian operator with diagonals
-        [-1.0, -1.0, -1.0, -1.0, 8.0, -1.0, -1.0, -1.0, -1.0],
-        |_x, _y, Rgb([r, g, b])| {
-            #[rustfmt::skip]
+    apply_kernel(image, LAPLACIAN, |_x, _y, [r, g, b]| {
+        #[rustfmt::skip]
             let luma =
                 0.212671 * r as f64 +
                 0.715160 * g as f64 +
                 0.072169 * b as f64;
 
-            average += weight * luma;
-            sumsq += luma * luma;
-        },
-    );
+        average += weight * luma;
+        sumsq += luma * luma;
+    });
 
     EdgeInfo {
         average,
         // NOTE: since weight is 1/n, this may be slightly biased
-        variance: (sumsq - (average * average) * num_pixels) * weight,
+        variance: (sumsq - (average * average) * n) * weight,
     }
 }
