7a81b30061d3e835122f919429dab5b8c5ec9025
novacrazy <novacrazy@gmail.com>
2023-01-12 07:09:10 -0600

    Rework http server for config changes

--- layers/server/src/tasks/http_server.rs
+++ layers/server/src/tasks/http_server.rs
@@ -1,8 +1,10 @@
+use futures::FutureExt;
 use hyper::{
     server::conn::AddrStream,
     service::{make_service_fn, service_fn},
     Server,
 };
+use std::convert::Infallible;
 
 use ftl::{Reply, StatusCode};
 
@@ -16,37 +18,67 @@ pub fn add_http_server_task(state: &ServerState, runner: &TaskRunner) {
 struct HttpServer(ServerState);
 
 impl task_runner::Task for HttpServer {
-    fn start(self, mut alive: tokio::sync::watch::Receiver<bool>) -> tokio::task::JoinHandle<()> {
+    fn start(self, alive: tokio::sync::watch::Receiver<bool>) -> tokio::task::JoinHandle<()> {
         let HttpServer(state) = self;
 
-        let server = Server::bind(&state.config.web.bind)
-            .http2_adaptive_window(true)
-            .tcp_nodelay(true)
-            .serve(make_service_fn(move |socket: &AddrStream| {
-                //let remote_addr = socket.get_ref().0.remote_addr();
-                let remote_addr = socket.remote_addr();
+        tokio::spawn(async move {
+            let mut bind_addr;
+
+            while *alive.borrow() {
                 let state = state.clone();
+                let mut alive = alive.clone();
 
-                futures::future::ok::<_, std::convert::Infallible>(service_fn(move |req| {
-                    let state = state.clone();
+                let mut config_changed = state.config_watcher.clone();
+                let (send_shutdown, shutdown) = tokio::sync::oneshot::channel::<()>();
 
-                    // gracefully fail and return HTTP 500
-                    async move {
-                        match tokio::spawn(crate::web::service::service(remote_addr, req, state)).await {
-                            Ok(resp) => resp,
-                            Err(err) => {
-                                log::error!("Internal Server Error: {err}");
+                bind_addr = state.config().web.bind;
 
-                                Ok(StatusCode::INTERNAL_SERVER_ERROR.into_response())
+                let controller = async move {
+                    loop {
+                        tokio::select! {
+                            biased;
+                            _ = alive.changed() => break, // send shutdown and kill controller loop
+                            value = config_changed.changed() => match value {
+                                Ok(_) if config_changed.borrow().web.bind == bind_addr => continue, // nothing changed, continue to poll
+                                _ => break, // send graceful shutdown to restart server
                             }
                         }
                     }
-                }))
-            }))
-            .with_graceful_shutdown(async move {
-                let _ = alive.changed().await;
-            });
 
-        tokio::spawn(async move { server.await.unwrap() })
+                    send_shutdown.send(())
+                };
+
+                let server = Server::bind(&bind_addr)
+                    .http2_adaptive_window(true)
+                    .tcp_nodelay(true)
+                    .serve(make_service_fn(move |socket: &AddrStream| {
+                        //let remote_addr = socket.get_ref().0.remote_addr();
+                        let remote_addr = socket.remote_addr();
+                        let state = state.clone();
+
+                        futures::future::ok::<_, Infallible>(service_fn(move |req| {
+                            let state = state.clone();
+
+                            // gracefully fail and return HTTP 500
+                            async move {
+                                match tokio::spawn(crate::web::service::service(remote_addr, req, state)).await {
+                                    Ok(resp) => resp,
+                                    Err(err) => {
+                                        log::error!("Internal Server Error: {err}");
+
+                                        Ok(StatusCode::INTERNAL_SERVER_ERROR.into_response())
+                                    }
+                                }
+                            }
+                        }))
+                    }))
+                    .with_graceful_shutdown(shutdown.map(|_| ()));
+
+                if let (Err(e), _) = tokio::join!(server, controller) {
+                    // Will be "caught" by RetryTask
+                    panic!("Hyper Server error: {e}");
+                }
+            }
+        })
     }
 }
