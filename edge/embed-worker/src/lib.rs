795d2977981ff4656b45b1bccaa60067690497c4
novacrazy <novacrazy@gmail.com>
2023-02-17 05:12:49 -0600

    Document hacks

--- edge/embed-worker/src/lib.rs
+++ edge/embed-worker/src/lib.rs
@@ -3,9 +3,13 @@ extern crate client_sdk as sdk;
 #[cfg(feature = "cf")]
 pub mod cf;
 
-pub static AVOID_OEMBED: phf::Set<&'static str> = phf::phf_set!("fxtwitter.com");
+pub static AVOID_OEMBED: phf::Set<&'static str> = phf::phf_set! {
+    // gives more generic information than the meta tags, so should be avoided
+    "fxtwitter.com"
+};
 
 // TODO: Add Lantern's user-agent to vxtwitter main
 pub static USER_AGENTS: phf::Map<&'static str, &'static str> = phf::phf_map! {
+    // https://github.com/dylanpdx/BetterTwitFix/blob/7a1c00ebdb6479afbfcca6d84450039d29029a75/twitfix.py#L35
     "vxtwitter.com" => "test",
 };
