8f225aae89045a0c4b2b86e84dd7c9add7565d65
novacrazy <novacrazy@gmail.com>
2023-02-17 18:16:11 -0600

    Build embed-worker in release script

--- scripts/_release.sh
+++ scripts/_release.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-SPLIT_RUSTFLAGS=$(echo $RUSTFLAGS | sed -E "s/\s+/\",\"/g")
+SHARED_FLAGS=" \
+    --config build.rustflags=[\"$(echo $RUSTFLAGS | sed -E "s/\s+/\",\"/g")\"] \
+    --config profile.release.strip=true \
+    --target $RUST_TARGET --release"
 
 echo "Building Lantern release for $RUST_TARGET"
 
-cross build \
-    --config "build.rustflags=[\"$SPLIT_RUSTFLAGS\"]" \
-    --config profile.release.strip=true \
-    --target $RUST_TARGET \
-    --bin process -p process --features binary --release || {
-        echo "Building Lantern asset processor failed"; exit 1;
-    }
+cross build $SHARED_FLAGS --bin embed-worker -p embed-worker || {
+    echo "Building Lantern embed-worker failed"; exit 1;
+}
 
-cross build \
-    --config "build.rustflags=[\"$SPLIT_RUSTFLAGS\"]" \
-    --config profile.release.strip=true \
-    --target $RUST_TARGET \
-    --bin main -p main --release || {
-        echo "Building Lantern server failed"; exit 1;
-    }
+cross build $SHARED_FLAGS --bin process -p process --features binary || {
+    echo "Building Lantern asset processor failed"; exit 1;
+}
+
+cross build $SHARED_FLAGS --bin main -p main || {
+    echo "Building Lantern server failed"; exit 1;
+}
