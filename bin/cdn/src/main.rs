bedb587db2ee014af7c00b381aa77287c8d3b2b4
novacrazy <novacrazy@gmail.com>
2024-10-28 15:49:55 -0500

    Start on CDN, using tokio_uring

--- bin/cdn/src/main.rs
+++ bin/cdn/src/main.rs
@@ -4,9 +4,33 @@
 
 extern crate tracing as log;
 
-#[tokio::main]
-async fn main() {
+pub mod state;
+
+pub mod prelude {
+    pub use crate::state::CdnServerState;
+    pub use common_web::error::Error;
+
+    pub use rpc::{auth::Authorization, event::ServerEvent};
+    pub use sdk::models::{aliases::*, Nullable, SmolStr, Snowflake, Timestamp};
+
+    pub type EventId = sdk::Snowflake;
+    pub type ConnectionId = sdk::Snowflake;
+
+    //pub use crate::config::Config; // TODO
+    pub use config::HasConfig;
+
+    pub use rkyv::Archived;
+}
+
+async fn run() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // NOTE: tokio_uring uses the Tokio current_thread runtime internally
+    tokio_uring::start(run())
 }
 
 pub mod built {
